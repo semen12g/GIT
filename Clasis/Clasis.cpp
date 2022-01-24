169532a44a6449d505ffdd0196a460642556d310
Semen <semen12vzx@mail.ru>
2022-01-24 16:03:42 +0900

    first commit

---
+++ Clasis/Clasis.cpp
@@ -0,0 +1,27 @@
+﻿#include "stdafx.h"
+#include <iostream>
+using namespace std;
+// начало объявления класса
+class CppStudio // имя класса
+{
+
+public:
+	void sides()const; //создание сторон
+	void coordinates()const; //создание координат на плоскости
+	void S()const; //S
+	void P()const; //P
+	
+private: // спецификатор доступа private
+	//int : x1, //длина 1 стороны
+		 // x2, //длина 2 стороны 
+		 // x3; //длина 3 стороны
+	double a; //сторона 1
+	double b; //сторона 2
+	double c; //сторона 3
+	double p; // периметр
+	double area; // площадь
+	double alpha; //угл1
+	double betta; //угл2
+	double gamma; //угл3
+};
+	
